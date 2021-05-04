98571819dd7eab2c881448444c495b2ad1780ebd
Pierre-Portfolio <60719810+Pierre-Portfolio@users.noreply.github.com>
2021-05-04 02:48:15 +0200

    add wpf , sql connection
    attention les image buggueront sur vos rep car coder en absolue je fix sa demain

--- VeloMax/VeloMax/MainWindow.xaml.cs
+++ VeloMax/VeloMax/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.ComponentModel;
+using MySql.Data.MySqlClient;
 
 namespace VeloMax
 {
@@ -20,9 +24,41 @@ namespace VeloMax
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        #region creation DB
+        public static MySqlConnection createDB()
+        {
+            MySqlConnection maConnexion = null;
+            maConnexion = new MySqlConnection("SERVER=localhost;PORT=3306;" + "DATABASE=velomax;" + "UID=root;PASSWORD=root;");
+            return maConnexion;
+            //maConnexion.Open();
+            //maConnexion.Close();
+        }
+        #endregion 
+
+        #region Variable Globale
+        // creation de l'objet mysql
+        MySqlConnection connection = createDB();
+
+        /*
+        // permet de savoir la fenetre actuel
+        // On crée toutes les pages dynamique
+        Grid DynamicGridClient = new Grid();
+        public DataGrid myGridClient = new DataGrid();
+        public DataGrid myGridCommis = new DataGrid();
+        public DataGrid myGridLivreur = new DataGrid();
+
+        Grid DynamicGridCommands = new Grid();
+        ListView ListeViewCommande = new ListView();
+        ComboBox cbRecherche = new ComboBox();
+
+        Grid DynamicGridStat = new Grid();
+        Grid DynamicGridAdmin = new Grid();
+        */
+        #endregion
         public MainWindow()
         {
-            InitializeComponent();
+        
         }
     }
 }
