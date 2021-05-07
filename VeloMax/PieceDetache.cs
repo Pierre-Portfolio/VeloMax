c614adc719344fdf8a6ba21f300d00a2782ca1aa
Pierre-Portfolio <60719810+Pierre-Portfolio@users.noreply.github.com>
2021-05-07 03:15:29 +0200

    Piece Detache add

---
+++ VeloMax/PieceDetache.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeloMax
+{
+    class PieceDetache
+    {
+        private string numpiece;
+        private string descpiece;
+        private int numprodcatalogue;
+        private float prixpiece;
+        private DateTime dateintroprod;
+        private DateTime datediscontprod;
+        private int delaiapprovprod;
+        private string siret;
+
+        public PieceDetache(string numpiece,string descpiece,int numprodcatalogue,float prixpiece, DateTime dateintroprod, DateTime datediscontprod,int delaiapprovprod,string siret)
+        {
+            this.numpiece = numpiece;
+            this.descpiece = descpiece;
+            this.numprodcatalogue = numprodcatalogue;
+            this.prixpiece = prixpiece;
+            this.dateintroprod = dateintroprod;
+            this.datediscontprod = datediscontprod;
+            this.delaiapprovprod = delaiapprovprod;
+            this.siret = siret;
+        }
+
+        public string Numpiece
+        {
+            get { return this.numpiece; }
+        }
+
+        public string Descpiece
+        {
+            get { return this.descpiece; }
+        }
+
+        public int Numprodcatalogue
+        {
+            get { return this.numprodcatalogue; }
+        }
+
+        public float Prixpiece
+        {
+            get { return this.prixpiece; }
+        }
+
+        public DateTime Dateintroprod
+        {
+            get { return this.dateintroprod; }
+        }
+
+        public DateTime Datediscontprod
+        {
+            get { return this.datediscontprod; }
+        }
+
+        public int Delaiapprovprod
+        {
+            get { return this.delaiapprovprod; }
+        }
+
+        public string Siret
+        {
+            get { return this.siret; }
+        }
+        
+    }
+}
