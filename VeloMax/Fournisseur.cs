a0844fb5baebf96b1c77626a2d2d72bc0d0e1b72
Pierre-Portfolio <60719810+Pierre-Portfolio@users.noreply.github.com>
2021-05-07 03:20:46 +0200

    add fournisseur class

---
+++ VeloMax/Fournisseur.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeloMax
+{
+    class Fournisseur
+    {
+        private string siret;
+        private string nomentreprise;
+        private string contact;
+        private string adrfour;
+        private string libellefourniseur;
+
+
+        public Fournisseur(string siret, string nomentreprise, string contact, string adrfour,string libellefourniseur)
+        {
+            this.siret = siret;
+            this.nomentreprise = nomentreprise;
+            this.contact = contact;
+            this.adrfour = adrfour;
+            this.libellefourniseur = libellefourniseur;
+        }
+
+        public string Siret
+        {
+            get { return this.siret; }
+        }
+
+        public string Nomentreprise
+        {
+            get { return this.nomentreprise; }
+        }
+
+        public string Contact
+        {
+            get { return this.contact; }
+        }
+
+        public string Adrfour
+        {
+            get { return this.adrfour; }
+        }
+
+        public string Libellefourniseur
+        {
+            get { return this.libellefourniseur; }
+        }
+
+        public string AfficherBoisson()
+        {
+            return this.Siret + " " + this.Nomentreprise + " " + this.Contact + " " + this.Adrfour + " " + this.Libellefourniseur;
+        }
+
+    }
+}
