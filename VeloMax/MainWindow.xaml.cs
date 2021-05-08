de08ea9cc95311750ada90d1ed1094cb7ea222a9
Pierre-Portfolio <60719810+Pierre-Portfolio@users.noreply.github.com>
2021-05-08 04:40:28 +0200

    add icon bicy

--- VeloMax/MainWindow.xaml.cs
+++ VeloMax/MainWindow.xaml.cs
@@ -157,7 +157,7 @@ namespace VeloMax
             Grid.SetColumnSpan(txtBlock1, 6);
             DynamicGridMateriel.Children.Add(txtBlock1);
 
-            // tableau des clients
+            // tableau des bicyclette
             myGridBicy.Items.Clear();
             myGridBicy.Width = 700;
             myGridBicy.Height = 100;
@@ -176,6 +176,7 @@ namespace VeloMax
                 key++;
             }
             myGridBicy.ItemsSource = myDictBicy.Values;
+            connection.Close();
 
             //on define le reste
             myGridBicy.Foreground = new SolidColorBrush(Colors.Orange);
@@ -188,6 +189,70 @@ namespace VeloMax
             Grid.SetColumn(myGridBicy, 0);
             Grid.SetColumnSpan(myGridBicy, 6);
             DynamicGridMateriel.Children.Add(myGridBicy);
+
+            //Btn ajouter
+            Button btnAddBicy = new Button();
+            btnAddBicy.Content = "";
+            btnAddBicy.Background = Brushes.Green;
+            btnAddBicy.Height = 15;
+            btnAddBicy.Width = 15;
+            Grid.SetRow(btnAddBicy, 3);
+            Grid.SetColumn(btnAddBicy, 0);
+            Grid.SetColumnSpan(btnAddBicy, 6);
+            btnAddBicy.BorderThickness = new Thickness(0, 0, 0, 0);
+            btnAddBicy.Margin = new Thickness(150, -141, 0, 0);
+            btnAddBicy.ToolTip = "Ajouter un client";
+            btnAddBicy.Background = new ImageBrush(new BitmapImage(new Uri(@"https://cdn.pixabay.com/photo/2014/04/02/10/41/button-304224_640.png")));
+            //btnAddClient.Click += new RoutedEventHandler(OpenAddClient);
+            DynamicGridMateriel.Children.Add(btnAddBicy);
+
+            //Btn modifier
+            Button btnModifBicy = new Button();
+            btnModifBicy.Content = "";
+            btnModifBicy.Background = Brushes.Green;
+            btnModifBicy.Height = 15;
+            btnModifBicy.Width = 15;
+            Grid.SetRow(btnModifBicy, 3);
+            Grid.SetColumn(btnModifBicy, 0);
+            Grid.SetColumnSpan(btnModifBicy, 6);
+            btnModifBicy.BorderThickness = new Thickness(0, 0, 0, 0);
+            btnModifBicy.Margin = new Thickness(200, -141, 0, 0);
+            btnModifBicy.ToolTip = "Modifier un client";
+            btnModifBicy.Background = new ImageBrush(new BitmapImage(new Uri(@"https://cdn.pixabay.com/photo/2016/03/29/06/22/edit-1287617_1280.png")));
+            //btnModifClient.Click += new RoutedEventHandler(ButtonModifClient);
+            DynamicGridMateriel.Children.Add(btnModifBicy);
+
+            //Btn del
+            Button btnSuprBicy = new Button();
+            btnSuprBicy.Content = "";
+            btnSuprBicy.Background = Brushes.Red;
+            btnSuprBicy.Height = 15;
+            btnSuprBicy.Width = 15;
+            Grid.SetRow(btnSuprBicy, 3);
+            Grid.SetColumn(btnSuprBicy, 0);
+            Grid.SetColumnSpan(btnSuprBicy, 6);
+            btnSuprBicy.BorderThickness = new Thickness(0, 0, 0, 0);
+            btnSuprBicy.ToolTip = "Supprimer un client";
+            btnSuprBicy.Background = new ImageBrush(new BitmapImage(new Uri(@"https://cdn.pixabay.com/photo/2013/07/12/17/00/remove-151678_960_720.png")));
+            btnSuprBicy.Margin = new Thickness(250, -141, 0, 0);
+            //btnSuprClient.Click += new RoutedEventHandler(ButtonSupClient);
+            DynamicGridMateriel.Children.Add(btnSuprBicy);
+
+            //Btn Rechercher
+            Button btnChercherBicy = new Button();
+            btnChercherBicy.Content = "";
+            btnChercherBicy.BorderThickness = new Thickness(0, 0, 0, 0);
+            btnChercherBicy.Background = new SolidColorBrush(Colors.White);
+            btnChercherBicy.Background = new ImageBrush(new BitmapImage(new Uri(@"https://www.icone-png.com/png/1/1402.png")));
+            btnChercherBicy.Height = 15;
+            btnChercherBicy.Width = 15;
+            btnChercherBicy.ToolTip = "Rechercher un client";
+            Grid.SetRow(btnChercherBicy, 3);
+            Grid.SetColumn(btnChercherBicy, 0);
+            Grid.SetColumnSpan(btnChercherBicy, 6);
+            btnChercherBicy.Margin = new Thickness(300, -141, 0, 0);
+            //btnChercher.Click += new RoutedEventHandler(OpenChercherClient);
+            DynamicGridMateriel.Children.Add(btnChercherBicy);
         }
 
         #endregion Generation DynamicGrid
@@ -246,9 +311,6 @@ namespace VeloMax
         {
             Refresh();
             MaterielBtn.Background = new SolidColorBrush(Colors.White);
-
-            //myGridClient.ItemsSource = l.Values;
-
             MainGrid.Children.Add(DynamicGridMateriel);
         }
 
