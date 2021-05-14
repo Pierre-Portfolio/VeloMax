49e6f444d67a16324ea620a7c0bcb1d1595d7f2d
Pierre-Portfolio <60719810+Pierre-Portfolio@users.noreply.github.com>
2021-05-14 16:14:10 +0200

    modifBicy

--- VeloMax/ModifBicy.xaml.cs
+++ VeloMax/ModifBicy.xaml.cs
@@ -64,11 +64,115 @@ namespace VeloMax
             }
             BoxligneProd.ItemsSource = listProduit;
             connection.Close();
+
+            BoxNom.Text = b.Nom;
+            BoxGrandeur.SelectedItem = b.Grandeur;
+            BoxPrix.SelectedText = b.Prixbicy.ToString();
+            BoxligneProd.SelectedItem = b.Ligneproduit;
+            BoxDateDisc.SelectedText = b.Datediscontinuationbicy.ToString();
+        }
+
+        private void BoxNom_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            if (BoxGrandeur.Text.ToString() == "")
+            {
+                connection.Open();
+                MySqlCommand command = connection.CreateCommand();
+                command.CommandText = "SELECT grandeur FROM velomax.assemblage where nom='" + BoxNom.SelectedItem.ToString() + "';";
+                MySqlDataReader reader = command.ExecuteReader();
+                List<string> listNom = new List<string>();
+                while (reader.Read())// parcours ligne par ligne
+                {
+                    listNom.Add(reader.GetValue(0).ToString());
+                }
+                connection.Close();
+                BoxGrandeur.Items.Refresh();
+                BoxGrandeur.ItemsSource = listNom;
+            }
+        }
+
+        private void BoxGrandeur_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            if (BoxNom.Text.ToString() == "")
+            {
+                connection.Open();
+                MySqlCommand command = connection.CreateCommand();
+                command.CommandText = "SELECT nom FROM velomax.assemblage where grandeur = '" + BoxGrandeur.SelectedItem.ToString() + "';";
+                MySqlDataReader reader = command.ExecuteReader();
+                List<string> listGrandeur = new List<string>();
+                while (reader.Read())// parcours ligne par ligne
+                {
+                    listGrandeur.Add(reader.GetValue(0).ToString());
+                }
+                connection.Close();
+                BoxNom.Items.Refresh();
+                BoxNom.ItemsSource = listGrandeur;
+            }
         }
 
         private void AjouterClient(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("haha");
+            DateTime res2;
+            if (DateTime.TryParse(BoxDateDisc.Text.ToString(), out res2))
+            {
+                if (BoxPrix.Text != "" && BoxPrix.Text.Length != 0)
+                {
+                    int res;
+                    if (int.TryParse(BoxPrix.Text.ToString(), out res))
+                    {
+                        if (res >= 0)
+                        {
+                            if (BoxligneProd.Text != "" && BoxGrandeur.Text.Length != 0)
+                            {
+                                connection.Open();
+                                MySqlCommand command = connection.CreateCommand();
+                                MessageBox.Show("UPDATE velomax.bicyclette SET prixbicy = '" + BoxPrix.SelectedText + "',ligneproduit = '" + BoxligneProd.SelectedItem + "', datediscontinuationbicy = '" + res2.ToString("yyyy-MM-dd HH:mm:ss") + "', nom = '" + BoxNom.SelectedItem + "', grandeur = '" + BoxGrandeur.SelectedItem + "' WHERE idbicy = " + b.Idbicy);
+                                command.CommandText = "UPDATE velomax.bicyclette SET prixbicy = '" + BoxPrix.SelectedText + "',ligneproduit = '" + BoxligneProd.SelectedItem + "', datediscontinuationbicy = '" + res2.ToString("yyyy-MM-dd HH:mm:ss") + "', nom = '" + BoxNom.SelectedItem + "', grandeur = '" + BoxGrandeur.SelectedItem + "' WHERE idbicy = " + b.Idbicy;
+                                MySqlDataReader reader = command.ExecuteReader();
+                                connection.Close();
+
+                                // on recupere les datas
+                                connection.Open();
+                                command = connection.CreateCommand();
+                                command.CommandText = "SELECT * FROM velomax.bicyclette;";
+                                reader = command.ExecuteReader();
+                                List<Bicyclette> myListBicy = new List<Bicyclette>();
+                                while (reader.Read())// parcours ligne par ligne
+                                {
+                                    myListBicy.Add(new Bicyclette(Convert.ToInt32(reader.GetValue(0).ToString()), reader.GetValue(1).ToString(), reader.GetValue(2).ToString(), Convert.ToInt32(reader.GetValue(3).ToString()), reader.GetValue(4).ToString(), Convert.ToDateTime(reader.GetValue(5)), Convert.ToDateTime(reader.GetValue(6))));
+                                    mw.keyBicy = Convert.ToInt32(reader.GetValue(0));
+                                }
+                                mw.myGridBicy.ItemsSource = myListBicy;
+                                mw.myGridBicy.Items.Refresh();
+                                connection.Close();
+
+                                this.Close();
+
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Erreur le champ Ligne Produit ne doit pas être vide!");
+                                }
+                            }
+                            else
+                            {
+                                MessageBox.Show("Erreur le champ prix doit etre positif !");
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Erreur le champ prix doit contenir que un nombre ronds !");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Erreur le champ prix est vide !");
+                    }
+            }
+            else
+            {
+                MessageBox.Show("Erreur , veuillez modifier la saisie de la date de discontinuité !");
+            }
         }
     }
 }
